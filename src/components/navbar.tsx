3ece28ee90cbdca4f25faabec97573129a6535fa
Iqro-dev <arumisokolowska@gmail.com>
2023-03-31 12:06:50 +0200

    feat(navbar): add menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,22 @@
 import { Box, AppBar, Toolbar, Typography } from '@mui/material'
+import { PropsWithChildren } from 'react'
 
-export default function Navbar() {
+export default function Navbar({ children }: PropsWithChildren) {
   return (
     <Box>
       <AppBar position='absolute'>
         <Toolbar
           variant='dense'
           sx={{
-            pr: '24px', // keep right padding when drawer closed
+            // keep right padding when drawer closed
             height: '48px',
+            display: 'flex',
+            justifyContent: 'space-between',
           }}
         >
           <Typography variant='h6'>Kursy przedmałżeńskie</Typography>
+
+          <Box>{children}</Box>
         </Toolbar>
       </AppBar>
     </Box>
