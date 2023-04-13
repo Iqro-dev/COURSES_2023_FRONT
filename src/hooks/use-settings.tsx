d92842211d4660fe9cbdf8c9d9fea044ef143b9a
Iqro-dev <arumisokolowska@gmail.com>
2023-04-13 15:59:01 +0200

    feat(settings): add settings to local storage

--- src/hooks/use-settings.tsx
+++ src/hooks/use-settings.tsx
@@ -10,8 +10,11 @@ interface SettingsResponse {
 export function useSettings() {
   const { getApiResponse } = useApi()
 
-  const [settings, setSettings] = useState<SettingsResponse>({ headerText: '', headerColor: '' })
-  const [loaded, setLoaded] = useState(false)
+  const [settings, setSettings] = useState<SettingsResponse>({
+    headerText: localStorage.getItem('headerText') ?? '',
+    headerColor: localStorage.getItem('headerColor') ?? '',
+  })
+  const [loaded, setLoaded] = useState(true)
 
   const load = () => {
     getApiResponse<SettingsResponse>('/settings', Methods.GET).then((res) => {
@@ -20,6 +23,9 @@ export function useSettings() {
       const settings = res.data
 
       if (!settings) return
+
+      localStorage.setItem('headerText', settings.headerText)
+      localStorage.setItem('headerColor', settings.headerColor)
 
       setSettings(settings)
       setLoaded(true)
