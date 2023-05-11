7cf71118a5ae9315a828c082a032c9237a42cd6a
Iqro-dev <arumisokolowska@gmail.com>
2023-05-11 11:59:01 +0200

    fix(auth): navigate when loginStatus is false

--- src/components/routes/auth-route.tsx
+++ src/components/routes/auth-route.tsx
@@ -1,19 +1,19 @@
 import { PropsWithChildren, ReactElement, useContext, useEffect } from 'react'
-// import AuthService from './Services/AuthService'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../providers/auth-provider'
 
 export function AuthRoute({ children }: PropsWithChildren): ReactElement {
   const navigate = useNavigate()
+
   const {
     auth: { loginStatus },
   } = useContext(AuthContext)
 
-  const validateAuth = () => {
-    if (typeof loginStatus !== 'undefined' && !loginStatus) navigate('/')
-  }
-
-  useEffect(validateAuth, [loginStatus])
+  useEffect(() => {
+    if (!loginStatus) {
+      navigate('/')
+    }
+  }, [location.pathname, loginStatus])
 
   return <>{children ?? <Outlet />}</>
 }
